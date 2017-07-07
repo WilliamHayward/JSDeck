114c2da7c63ba74af8214f87f5e36e5774764272
William Hayward <haywardwilliam1996@gmail.com>
2017-07-07 11:25:38 +1000

    +Completed tests for vital components

--- test/card.js
+++ test/card.js
@@ -1,12 +1,29 @@
 var assert = require('assert');
-var jsdeck = require('../jsdeck.js');
+var JSDeck = require('../jsdeck.js');
 
 describe ('Deck', function() {
     it ('Should allow cards to be created', function() {
+        var card = new JSDeck.Card("1");
+        assert.equal(card.value, "1");
 
+        card = new JSDeck.Card(2);
+        assert.equal(card.value, 2);
+
+        card = new JSDeck.Card([1, 2, 3]);
+        assert.deepEqual(card.value, [1, 2, 3]);
     });
 
     it ('Should allow cards to be copied', function() {
+        var card1 = new JSDeck.Card("1");
+        var card2 = new JSDeck.Card(2);
+        var card3 = new JSDeck.Card([1, 2, 3]);
 
+        var clone1 = new JSDeck.Card(card1);
+        var clone2 = new JSDeck.Card(card2);
+        var clone3 = new JSDeck.Card(card3);
+        
+        assert.equal(clone1.value, "1");
+        assert.equal(clone2.value, 2);
+        assert.deepEqual(clone3.value, [1, 2, 3]);
     });
 });
--- test/deck.js
+++ test/deck.js
@@ -1,51 +1,146 @@
 var assert = require('assert');
-var jsdeck = require('../jsdeck.js');
+var JSDeck = require('../jsdeck.js');
 
 describe('Deck', function() {
     it ('Should be able to create a new deck', function() {
-        var deck = new jsdeck.Deck();
+        var deck = new JSDeck();
         assert.equal(deck.size(), 0);
 
-        deck = new jsdeck.Deck([
+        deck = new JSDeck([
             "1", "2", "3"
         ]);
         assert.equal(deck.size(), 3);
         
-        deck = new jsdeck.Deck([
-            new Card("1"),
-            new Card("2"),
-            new Card("3"),
-            new Card("4")
+        deck = new JSDeck([
+            new JSDeck.Card("1"),
+            new JSDeck.Card("2"),
+            new JSDeck.Card("3"),
+            new JSDeck.Card("4")
         ]);
         assert.equal(deck.size(), 4);
     });
 
     it ('Should be able to add new cards to the deck', function() {
-        var deck = new jsdeck.Deck();
+        var deck = new JSDeck();
         assert.equal(deck.size(), 0);
 
-        var card = new jsdeck.Card();
+        var card = new JSDeck.Card("Apple");
+        deck.add(card);
+        assert.equal(deck.size(), 1);
 
-        //TODO different quanitities
+        card = new JSDeck.Card("Peach");
+        deck.add(card, 2);
+        assert.equal(deck.size(), 3);
+
+        deck.add("Banana");
+        assert.equal(deck.size(), 4);
+
+        deck.add("Pear", 2);
+        assert.equal(deck.size(), 6);
+
+        deck.add("Strawberry", 1000);
+        assert.equal(deck.size(), 1006);
     });
 
     it ('Should be able to remove cards from the deck', function() {
-
+        // TODO: This is a very low priority
     });
 
     it ('Should shuffle the entire deck', function() {
+        // TODO: Revise these tests
+        var deck = new JSDeck();
+        deck.add("Apple");
+        deck.add("Pear", 9999);
 
+        deck.shuffle();
+        card = deck.draw();
+        assert.equal(card, "Pear");
+        assert.equal(deck.size(), 10000);
+        assert.equal(deck.remaining(), 9999);
+
+        deck.shuffle();
+        assert.equal(deck.size(), 10000);
+        assert.equal(deck.remaining(), 10000);
+
+        // NOTE: Because of randomization, there is a 1 / 10000 chance of this test failling on accident
+        
     });
 
     it ('Should shuffle remaining cards', function() {
+        // TODO: Revise these tests
+        var deck = new JSDeck();
+        deck.add("Apple", 3);
+        deck.add("Pear", 9999);
+        deck.draw(2);
 
+        deck.shuffleRemaining();
+        card = deck.draw();
+        assert.equal(card, "Pear");
+        assert.equal(deck.remaining(), 9999);
+
+        deck.shuffleRemaining();
+        assert.equal(deck.remaining(), 9999);
+
+        // NOTE: Because of randomization, there is a 1 / 10000 chance of this test failling on accident
     });
 
     it ('Should allow cards to be drawn', function() {
+        var deck = new JSDeck([
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"
+        ]);
 
+        var card = deck.draw();
+        assert.equal(card, "1");
+        assert.equal(deck.remaining(), 5); // 5 cards remaining
+        assert.equal(deck.size(), 6); // SHould not effect total size of deck
+
+        var cards = deck.draw(2);
+        assert.deepEqual(cards, ["2", "3"]);
+        assert.equal(deck.remaining(), 3); // 3 cards remaining
+        assert.equal(deck.size(), 6); // SHould not effect total size of deck
+
+        cards = deck.draw(1); // When given an argument (even 1), should ALWAYS return array
+        assert.deepEqual(cards, ["4"]);
+        assert.equal(deck.remaining(), 2); // 2 cards remaining
+        assert.equal(deck.size(), 6); // SHould not effect total size of deck
+
+        cards = deck.draw(0);
+        assert.deepEqual(cards, []);
+        assert.equal(deck.remaining(), 2); // 2 cards remaining
+        assert.equal(deck.size(), 6); // SHould not effect total size of deck
+
+        cards = deck.draw(-1); // Negative values should be treated as "0"
+        assert.deepEqual(cards, []);
+        assert.equal(deck.remaining(), 2); // 2 cards remaining
+        assert.equal(deck.size(), 6); // SHould not effect total size of deck
     });
 
     it ('Should allow cards to be returned', function() {
+        // TODO: Not a priority
+    });
+
+    it ('Should allow the deck to be reset (without shuffling)', function() {
+        var deck = new JSDeck([
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"
+        ]);
+
+        deck.draw(3);
+
+        assert.equal(deck.remaining(), 3);
+
+        deck.reset();
+
+        assert.equal(deck.remaining(), 6);
 
     });
 });
